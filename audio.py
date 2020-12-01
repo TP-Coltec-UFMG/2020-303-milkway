983058a1d23ffd0bca93c824fcb894bba2c8f461
Erick Rocha <erickfr1011@gmail.com>
2020-12-01 19:23:47 -0300

    Arquivos de som para teste Arquivos com recursos para o jogo serao guardados no diretorio assets. A funcao make_sounds so funciona com alguns arquivos.

--- audio.py
+++ audio.py
@@ -1,5 +1,5 @@
 import openal as oal
-# import time
+import time
 
 
 def make_sounds(positions, sounds):
@@ -14,5 +14,14 @@ def make_sounds(positions, sounds):
         sources.append(source)
     for source in sources:
         source.play()
-    # time.sleep(3)
-    # oal.oalQuit()
+    time.sleep(3)
+    oal.oalQuit()
+
+
+# Por algum motivo so esse house_lo.ogg funciona o som 3d
+
+make_sounds([(-2, 0, 0)], ["assets/house_lo.ogg"])
+make_sounds([(2, 0, 0)], ["assets/house_lo.ogg"])
+make_sounds([(-2, -2, -2)], ["assets/bip2.ogg"])
+make_sounds([(2, 2, 2)], ["assets/bip2.ogg"])
+# make_sounds([(0, 0, -1), (4, 0, 0)], ["assets/bip1.ogg", "assets/bip2.ogg"])
