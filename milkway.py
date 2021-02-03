efc9154bb8d9b09659857488224e9038add012ce
Erick Rocha <erickfr1011@gmail.com>
2021-02-02 21:09:19 -0300

    commit que possivelmente estragará tudo

--- milkway.py
+++ milkway.py
@@ -144,6 +144,29 @@ def Blind_Game_Start():
     """
     Game_Start(True)
 
+def Instructions():
+
+    run = True
+
+    while run:
+
+        # desenha fundo
+        instructions_bg = pygame.image.load("assets/images/instrucoes1.jpg")
+        surface.blit(instructions_bg, (0, 0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    Game_Start()
+                if event.key == pygame.K_LEFT:
+                    menu.mainloop(surface)
+
+        pygame.display.update()
+
+    pygame.quit()
 
 # Carrega imagem de fundo do Menu
 myimage = pygame_menu.baseimage.BaseImage(
@@ -167,7 +190,7 @@ mytheme = pygame_menu.themes.Theme(
 # Coloca o menu na Tela
 menu = pygame_menu.Menu(screen_height, screen_width, 'MilkWay', theme=mytheme)
 menu.add_button('Jogar', Game_Start)
-menu.add_button('Instrucoes', pygame_menu.events.EXIT)
+menu.add_button('Instrucoes', Instructions)
 menu.add_button('Blind Mode', Blind_Game_Start)
 menu.add_button('Sair', pygame_menu.events.EXIT)
 
